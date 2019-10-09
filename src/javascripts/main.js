d40a4cb0ad0707316e47573a0bab4cd3d8a43d7a
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-10-08 20:27:34 -0500

    added fight quad

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import '../styles/main.scss';
 import eat from './components/eat/eat';
 import play from './components/play/play';
+import fight from './components/fight/fight';
 
 const init = () => {
   eat.eatTomQuad();
   play.playTomQuad();
+  fight.fightTomQuad();
 };
 
 init();
